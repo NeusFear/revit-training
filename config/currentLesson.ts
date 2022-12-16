222cbb9b654c1a33e9ae6f21533c920ad97d629c
AWIRO2020 <62273244+AWIRO2020@users.noreply.github.com>
2022-12-16 12:35:16 -0600

    Update currentLesson.ts

--- config/currentLesson.ts
+++ config/currentLesson.ts
@@ -1,4 +1,4 @@
 //The number of the latest released lesson
 //Example if set to 10 all lessons 0-10 will be available on the website
 //As of 12-14-2022 the max number of lessons is 34 ending with "Learn Revit Parametrics 3"
-export const releasedLessons = 34;
+export const releasedLessons = 02;
